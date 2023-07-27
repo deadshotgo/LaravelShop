8d0cb233c10a019b2f16d9c944d706390ec92c35
Denys <hromosoma235@gmail.com>
2023-07-27 21:31:05 +0200

    test

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/test', function () {
-    return '[privet][sve t]';
+    return '[privet][svelolool]';
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
