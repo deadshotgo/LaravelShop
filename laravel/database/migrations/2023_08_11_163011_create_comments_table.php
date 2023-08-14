9305deb5a05cde9dd753ccf054d2c0a52340c2f6
Denys <hromosoma235@gmail.com>
2023-08-14 12:56:14 +0200

    add routes, change on delete in db

--- laravel/database/migrations/2023_08_11_163011_create_comments_table.php
+++ laravel/database/migrations/2023_08_11_163011_create_comments_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->string('text');
-            $table->foreignId('blog_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('blog_id')->constrained()->onDelete('no action');
+            $table->foreignId('user_id')->constrained()->onDelete('no action');
             $table->timestamps();
         });
     }
